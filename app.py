40d39938bbf98f081ef00d985b46e5b131b923f4
Sudhir-Nishad <sudhirnishad271@gmail.com>
2024-12-26 19:14:23 +0530

    done

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import joblib
 import os
 
 app = Flask(__name__)
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static', 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 # Load the model and scaler
 model = joblib.load('trained_model.pkl')
